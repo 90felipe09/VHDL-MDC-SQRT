9a618bebc0f19ed1d23bc9cb3f2dcb4f18cf1c81
Kenzo Shiraishi <fkspoli@usp.br>
2018-11-15 16:46:03 -0200

    "square"

--- square_novo.vhd
+++ square_novo.vhd
@@ -22,31 +22,32 @@ architecture square_arch of square is
 			itera : 	in bit;
 			done : 	in bit;
 			reset : 	in bit;
-			
+
 			square : out signed (7 downto 0);
 			QX : 		out signed (7 downto 0);
-			S : 		out signed (7 downto 0);
+			S : 		out signed (7 downto 0)
 		);
 	end component;
-	
+
 	component square_uc is
 		port
 		(
 			square : in signed(7 downto 0);
 			QX : 		in signed(7 downto 0);
 			clk : 	in bit;
-			
+
 			itera : 	out bit;
-			done : 	out bit;
+			done : 	out bit
 		);
 	end component;
-	
-	signal itera, donesig, squaresig, qxsig;
-	
+
+	signal itera, donesig : bit;
+  signal squaresig, qxsig: signed (7 downto 0);
+
 	begin
 		fd: square_fd port map(X, clk, itera, donesig, reset, squaresig, qxsig, S);
-		uc: square_uc port map(sqauresig, qxsig, clk, reset, itera, done);
-		
+		uc: square_uc port map(squaresig, qxsig, clk, itera, done);
+
 end architecture;
 
 
@@ -62,29 +63,29 @@ entity square_uc is
 		square : in signed(7 downto 0);
 		QX : 		in signed(7 downto 0);
 		clk : 	in bit;
-		
-		itera : 	out bit;
-		done : 	out bit;
-  );
-  
-end entity square_uc;
 
+		itera : 	out bit;
+		done : 	out bit
+  );
+
+end entity square_uc;
 -- Descrição comportamental de UC
 ---- Estado: IMPLEMENTADO.
-architecture square_uc_arch of square is
-	my_process : process(clk) is
-		begin:
-			if (clk'event and clk='1') then
-				if (square <= QX) then 
-					itera <= '1';
-					concluiu <= '0';
-				else
-					itera <= '0';
-					concluiu <= '1';
-				end if;
-			end if;
-		end process my_process;
-		
+architecture square_uc_arch of square_uc is
+  begin
+  	my_process : process(clk) is
+  		begin
+  			if (clk'event and clk='1') then
+  				if (square <= QX) then
+  					itera <= '1';
+  					done <= '0';
+  				else
+  					itera <= '0';
+  					done <= '1';
+  				end if;
+  			end if;
+  		end process my_process;
+
 end architecture;
 
 
@@ -102,54 +103,54 @@ entity square_df is
 		itera : 	in bit;
 		done : 	in bit;
 		reset : 	in bit;
-		
+
 		square : out signed (7 downto 0);
 		QX : 		out signed (7 downto 0);
-		S : 		out signed (7 downto 0);
+		S : 		out signed (7 downto 0)
   );
-  
+
 end entity square_df;
 
 -- Descrição Estrutural de FD
 ---- Estado: Implementado.
-architecture square_df_arch of square is
+architecture square_df_arch of square_df is
 	component reg8bit is
     port (
       D : in signed (7 downto 0);
 		clk : in bit;
 		load : in bit;
 		shiftR : in bit;
-		
-		Do : out signed (7 downto 0);
+
+		Do : out signed (7 downto 0)
     );
 	end component;
-	
+
 	component somador is
     port (
       A : in signed (7 downto 0);
 		B : in signed (7 downto 0);
 		sel : in bit;
-		
-		R : out signed (7 downto 0);
+
+		R : out signed (7 downto 0)
     );
 	end component;
-	
+
 	component mux is
     port (
       A : in signed (7 downto 0);
 		B : in signed (7 downto 0);
 		sel : in bit;
-		
-		S : out signed (7 downto 0);
+
+		S : out signed (7 downto 0)
     );
 	end component;
-	
+
 	component buffertristate is
     port (
       A : in signed (7 downto 0);
 		sel : in bit;
-		
-		S : out signed (7 downto 0);
+
+		S : out signed (7 downto 0)
     );
 	end component;
 	-- s: saida
@@ -159,25 +160,26 @@ architecture square_df_arch of square is
 	---- S: Square
 	---- D: Delta
 	---- F: Final
-	signal sMS, sMD, sMF, sRS, sRD, sSA, sSB;
-	
-	begin:
+	signal sMS, sMD, sMF, sRS, sRD, sSA, sSB : signed (7 downto 0);
+
+	begin
 		RA: reg8bit port map (X, clk, itera, '0', QX);
 		RS: reg8bit port map(sMS, clk, itera, '0', sRS);
 		RD: reg8bit port map(sMD, clk, itera, done, sRD);
 		MS: mux port map(sSA, "00000001", reset, sMS);
 		MD: mux port map(sSB, "00000010", reset, sMD);
-		MF: mux port map("00000010", "00000001", sMF);
+		MF: mux port map("00000010", "00000001", done,sMF);
 		SA: somador port map(sRS, sRD, '0', sSA);
 		SB: somador port map(sRD, sMF, done, sSB);
 		BF: buffertristate port map(sSB, done, S);
-		
+
 		square <= sRS;
-		
+
 end architecture;
 
 library ieee;
 use ieee.numeric_bit.all;
+--use ieee.numeric_std.all;
 
 -- @brief reg8bit
 ---- Estado: Não implementado.
@@ -187,39 +189,80 @@ entity reg8bit is
 		clk : in bit;
 		load : in bit;
 		shiftR : in bit;
-		
-		Do : out signed (7 downto 0);
-  );
-  
-end entity square_df;
 
-architecture
+		Do : out signed (7 downto 0)
+  );
+
+end entity reg8bit;
+
+architecture reg8bit_arch of reg8bit is
 	signal Q : signed (7 downto 0);
 	begin
 		process(clk,load)
 			begin
-				if (clk'event = '1' and clk ='1') then
+				if (clk'event and clk ='1') then
 					if (load = '1') then
 						Q <= D;
 					elsif (shiftR = '1' and load = '0') then
-						Q <= Q slr 1;
+						Q <= shift_right(signed(Q), 1);
 					end if;
+        Do <= Q;
 				end if;
 			end process;
-		
-		
 
 end architecture;
 
 
+library ieee;
+use ieee.numeric_bit.all;
+
+entity somador is
+  port (
+    A : in signed (7 downto 0);
+    B : in signed (7 downto 0);
+    sel : in bit;
+
+    R : out signed (7 downto 0)
+  );
+end entity somador;
+
+architecture somador_arch of somador is
+  begin
+    R <= (A + B) when (sel = '0') else (A - B);
+end architecture;
+
+library ieee;
+use ieee.numeric_bit.all;
+
+entity mux is
+  port (
+    A : in signed (7 downto 0);
+    B : in signed (7 downto 0);
+    sel : in bit;
+
+    S : out signed (7 downto 0)
+  );
+end entity mux;
+
+architecture mux_arch of mux is
+  begin
+    S <= A when (sel = '0') else B;
+  end architecture;
 
 
+library ieee;
+use ieee.numeric_bit.all;
 
+entity buffertristate is
+  port(
+    A : in signed (7 downto 0);
+    sel : in bit;
 
+    S : out signed (7 downto 0)
+  );
+end entity buffertristate;
 
-
-
-
-
-
-
+architecture buffertristate_arch of buffertristate is
+  begin
+    S <= "00000000" when (sel = '0') else A;
+end architecture;

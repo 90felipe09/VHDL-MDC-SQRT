ce064c7fc14035e0b61c136426c55707e1272572
Felipe Kenzo Shiraishi <90felipe09@users.noreply.github.com>
2018-11-23 15:17:49 -0200

    Update teste.vhd

--- teste.vhd
+++ teste.vhd
@@ -1,362 +1,272 @@
-	library ieee;
-	use ieee.numeric_bit.all;
+--------------------------------------------------------------------------------
+--! 5307474 Bruno de Carvalho Albertini
+--------------------------------------------------------------------------------
 
+library ieee;
+use ieee.numeric_bit.all;
 
-	-- @brief Raiz Quadrada
-	entity square is
-	  port (
-		 X     : in  signed(7 downto 0); -- entrada
-		 S     : out signed(7 downto 0); -- saida
-		 reset : in  bit; -- reset ativo alto assíncrono
-		 --done  : out bit; -- alto quando terminou de calcular
-		 clk   : in  bit;
-		 q : out signed(1 downto 0)
-	  );
-	end entity square;
+-- @brief Multiplexador
+---- Estado: Implementado.
+entity Multiplexador is
+  port (
+		Entrada1 : in signed (7 downto 0);
+		Entrada2 : in signed (7 downto 0);
+		Seletora : in bit;
 
+		Saida    : out signed (7 downto 0)
+  );
 
-	architecture arch of square is
-	--Adicionando componentes
-	  component square_con is
-	  port (
-			aletb : in  bit;
-			loadSaida : out  bit;
-			loadSquare : out  bit;
-			loadDelta  : out  bit;
-			setIn      : out  bit;
-			setSub     : out  bit;
-			reset : in  bit; -- reset ativo alto assíncrono
-			--done  : out bit; -- alto quando terminou de calcular
-			clk   : in  bit;
-			q : out signed(1 downto 0)
-			);
-	  end component;
+end entity Multiplexador;
 
-	  component square_fd is
-	  port (
-			X     : in  signed(7 downto 0); -- entrada
-			S     : out signed(7 downto 0); -- saida
-			aletb : out  bit;
-			loadSaida : in  bit;
-			loadSquare : in  bit;
-			loadDelta  : in  bit;
-			setIn      : in  bit;
-			setSub     : in  bit;
-			clk   : in  bit;
-			reset : in bit
-	  );
-	  end component;
+architecture Multiplexador_arch of Multiplexador is
+	begin
+		Saida <= Entrada1 when (Seletora = '0') else
+             Entrada2 when (Seletora = '1') else
+             "11111111";
 
-	  signal aletb, loadSaida, loadSquare, loadDelta,setIn,setSub: bit;
+end architecture;
 
+library ieee;
+use ieee.numeric_bit.all;
+
+-- @brief Registrador
+---- Estado: Implementado.
+entity Registrador is
+  port (
+		Entrada : in signed (7 downto 0);
+    load : in bit;
+		clock : in bit;
+
+		Saida    : out signed (7 downto 0)
+  );
+
+end entity Registrador;
+
+architecture Registrador_arch of Registrador is
+	begin
+		process(clock, load)
+    begin
+      if (clock'event and clock = '1') then
+        if (load = '1') then
+          Saida <= Entrada;
+        end if;
+      end if;
+    end process;
+
+end architecture;
+
+library ieee;
+use ieee.numeric_bit.all;
+
+-- @brief Comparador
+---- Estado: Implementado.
+entity Comparador is
+  port (
+		EntradaA : in signed (7 downto 0);
+    EntradaB : in signed (7 downto 0);
+
+    AmaiorB  : out bit
+  );
+
+end entity Comparador;
+
+architecture Comparador_arch of Comparador is
 	begin
 
-		fd: square_fd port map(X,S,aletb,loadSaida,loadSquare,loadDelta,setIn,setSub,clk,reset);
-		uc: square_con port map(aletb,loadSaida,loadSquare,loadDelta,setIn,setSub,reset,clk,q);
-	end architecture;
+    AmaiorB <= '1' when (EntradaA > EntradaB) else
+               '0' when (EntradaA <= EntradaB) else
+               '0';
 
-	library ieee;
-	use ieee.numeric_bit.all;
+end architecture;
 
-	--Unidade de controle
-	entity square_con is
-	  port (
-			aletb : in  bit;
-			loadSaida : out  bit;
-			loadSquare : out  bit;
-			loadDelta  : out  bit;
-			setIn      : out  bit;
-			setSub     : out  bit;
-			reset : in  bit; -- reset ativo alto assíncrono
-			--done  : out bit; -- alto quando terminou de calcular
-			clk   : in  bit;
-			q : out signed(1 downto 0)
-	  );
-	end entity square_con;
+library ieee;
+use ieee.numeric_bit.all;
 
-	architecture uc of square_con is
+-- @brief Somador
+---- Estado: Implementado.
+entity Somador is
+  port (
+		Entrada1 : in signed (7 downto 0);
+    Entrada2 : in signed (7 downto 0);
+		Seletora : in bit;
 
-		type st is (caso_a,caso_b,caso_c,fim);
-		signal  estado : st;
+		Saida    : out signed (7 downto 0)
+  );
+
+end entity Somador;
+
+architecture Somador_arch of Somador is
 	begin
-		 asm : process(clk,reset,aletb)
-		 begin
-			if reset = '1' then
-			  estado <= caso_a;
-			elsif(clk'event and clk = '1') then
-			  case estado is
-				 when caso_a =>
-				 if aletb  = '1' then
-					estado <= caso_b;
-				 else
-					estado <= caso_c;
-				 end if;
+		Saida <= (Entrada1 + Entrada2) when (Seletora = '0') else
+             (Entrada1 - Entrada2) when (Seletora = '1') else
+             "11111111";
 
-				 when caso_b =>
-				 if aletb  = '1' then
-					estado <= caso_b;
-				 else
-					estado <= caso_c;
-				 end if;
+end architecture;
 
-				 when caso_c =>
-				 estado <= fim;
+library ieee;
+use ieee.numeric_bit.all;
 
-				 when fim =>
-					estado <= fim;
-				 end case;
-			  end if;
-			end process;
+-- @brief ShiftRight
+---- Estado: Implementado.
+entity ShiftRight is
+  port (
+		Entrada : in signed (7 downto 0);
 
-			process(estado)
-			begin
-			 case estado is
-			 when caso_a =>
-				loadSaida  <= '0';
-				loadSquare <= '1';
-				loadDelta  <= '1';
-				setIn      <= '1';
-				setSub     <= '0';
-				--done       <= '0';
-				q <= "00";
-			 when caso_b =>
-				loadSaida  <= '0';
-				loadSquare <= '1';
-				loadDelta  <= '1';
-				setIn      <= '0';
-				setSub     <= '0';
-				--done       <= '0';
-				q <= "01";
-			 when caso_c =>
-				loadSaida  <= '1';
-				loadSquare <= '0';
-				loadDelta  <= '0';
-				setIn      <= '0';
-				setSub     <= '1';
-				--done       <= '0';
-				q <= "10";
-			 when fim =>
-				loadSaida  <= '0';
-				loadSquare <= '0';
-				loadDelta  <= '0';
-				setIn      <= '0';
-				setSub     <= '0';
-				--done       <= '1';
-				q <= "11";
-			 end case;
-			end process;
+		Saida    : out signed (7 downto 0)
+  );
 
-	end architecture;
+end entity ShiftRight;
 
-
-	library ieee;
-	use ieee.numeric_bit.all;
-	--Fluxo de dados
-
-	entity square_fd is
-	  port (
-			X     : in  signed(7 downto 0); -- entrada
-			S     : out signed(7 downto 0); -- saida
-			aletb : out  bit;
-			loadSaida : in  bit;
-			loadSquare : in  bit;
-			loadDelta  : in  bit;
-			setIn      : in  bit;
-			setSub     : in  bit;
-			clk   : in  bit;
-			reset : in bit
-	  );
-	end entity square_fd;
-
-	architecture fd of square_fd is
-
-	--componentes
-		 component somador8bits
-	  port (
-		 A : in  signed(7 downto 0);
-		 B : in  signed(7 downto 0);
-		 S : out signed(7 downto 0)
-	  );
-	  end component somador8bits;
-
-	  component somaSub8Bits
-	port (
-	  A      : in  signed(7 downto 0);
-	  B      : in  signed(7 downto 0);
-	  setSub : in  bit;
-	  S      : out signed(7 downto 0)
-	);
-	end component somaSub8Bits;
-
-	component comparador
-	port (
-	  A : in  signed(7 downto 0);
-	  B : in  signed(7 downto 0);
-	  S : out bit
-	);
-	end component comparador;
-
-	component reg8
-	port (
-	  E     : in  signed(7 downto 0);
-	  S     : out signed(7 downto 0);
-	  load  : in  bit;
-	  reset : in  bit;
-	  clk   : in  bit
-	);
-	end component reg8;
-
-	component mux2to1
-	port (
-	  SEL : in  bit;
-	  A   : in  signed(7 downto 0);
-	  B   : in  signed(7 downto 0);
-	  S   : out signed(7 downto 0)
-	);
-	end component mux2to1;
-
-
-	component shift2
-	port (
-	  E : in  signed(7 downto 0);
-	  S : out signed(7 downto 0)
-	);
-	end component shift2;
-
-
-	signal square,delta,saida,somaA,somaB,squareIn,deltaIn,saidaIn : signed(7 downto 0);
-
-		begin
-	  somadorA: somador8bits port map(square, delta, somaA);
-	  somadorB: somaSub8Bits port map(delta, "00000010",setSub,somaB);
-	  compara: comparador port map(X, square, aletb);
-	  mux1: mux2to1 port map(setIn,somaA,"00000001",squareIn);
-	  mux2: mux2to1 port map(setIn,somaB,"00000011",deltaIn);
-	  squareReg: reg8 port map(squareIn,square,loadSquare,reset,clk);
-	  deltaReg: reg8 port map(deltaIn,delta,loadDelta,reset,clk);
-	  saidaReg: reg8 port map(saidaIn,S,loadSaida,reset,clk);
-	  shift: shift2 port map(somaB,saidaIn);
-	  
-	end architecture;
-
-
-	library ieee;
-	use ieee.numeric_bit.all;
-
-	entity somador8bits is
-	  port (
-			A : in  signed(7 downto 0);
-			B : in  signed(7 downto 0);
-			S : out  signed(7 downto 0)
-	  );
-	end entity somador8bits;
-
-	architecture hardware of somador8bits is
+architecture ShiftRight_arch of ShiftRight is
 	begin
-		S <= A + B;
-	end architecture;
+		Saida <= shift_right(signed(Entrada), 1);
 
-	library ieee;
-	use ieee.numeric_bit.all;
+end architecture;
 
-	entity somaSub8Bits is
-	  port (
-			A : in  signed(7 downto 0);
-			B : in  signed(7 downto 0);
-			setSub: in bit;
-			S : out  signed(7 downto 0)
-	  );
-	end entity somaSub8Bits;
+library ieee;
+use ieee.numeric_bit.all;
 
-	architecture hardware of somaSub8Bits is
-	begin
-		process(setSub,A,B)
-		begin
-			if setSub = '0' then
-				S <= A + B;
-			else
-				S <= A - B;
+-- @brief Raiz Quadrada
+entity square is
+  port (
+    X     : in  signed(7 downto 0); -- entrada
+    S     : out signed(7 downto 0); -- saida
+    reset : in  bit; -- reset ativo alto assíncrono
+    done  : out bit; -- alto quando terminou de calcular
+    clk   : in  bit
+  );
+end entity square;
+
+architecture square_arch of square is
+  component square_fd is
+    port (
+      A : in signed (7 downto 0);
+      clock : in bit;
+      itera : in bit;
+      done : in bit;
+      reset : in bit;
+
+      SgtA : out bit;
+      Saida : out signed (7 downto 0)
+    );
+  end component;
+  component square_uc is
+    port (
+      SgtA : in bit;
+      clock : in bit;
+
+      itera : out bit;
+      done : out bit
+    );
+  end component;
+
+  signal iteraUC, doneUC,SgtAFD : bit;
+
+  begin
+  fd: square_fd port map(X, clk, iteraUC, doneUC, reset, SgtAFD, S);
+  uc: square_uc port map(SgtAFD, clk, iteraUC, doneUC);
+  
+  done <= doneUC;
+end architecture;
+
+library ieee;
+use ieee.numeric_bit.all;
+
+-- @brief Raiz Quadrada
+entity square_fd is
+  port (
+    A : in signed (7 downto 0);
+    clock : in bit;
+    itera : in bit;
+    done : in bit;
+    reset : in bit;
+
+    SgtA : out bit;
+    Saida : out signed (7 downto 0)
+  );
+end entity square_fd;
+
+architecture square_fd_arch of square_fd is
+  component Registrador is
+    port (
+      Entrada : in signed (7 downto 0);
+      load : in bit;
+      clock : in bit;
+
+      Saida    : out signed (7 downto 0)
+    );
+  end component;
+  component Multiplexador is
+    port (
+      Entrada1 : in signed (7 downto 0);
+      Entrada2 : in signed (7 downto 0);
+      Seletora : in bit;
+
+      Saida    : out signed (7 downto 0)
+    );
+  end component;
+  component Somador is
+    port (
+      Entrada1 : in signed (7 downto 0);
+      Entrada2 : in signed (7 downto 0);
+      Seletora : in bit;
+
+      Saida    : out signed (7 downto 0)
+    );
+  end component;
+  component Comparador is
+    port (
+      EntradaA : in signed (7 downto 0);
+      EntradaB : in signed (7 downto 0);
+
+      AmaiorB  : out bit
+    );
+  end component;
+  component ShiftRight is
+    port (
+      Entrada : in signed (7 downto 0);
+
+      Saida    : out signed (7 downto 0)
+    );
+  end component;
+  signal saidaMux1, saidaMux2, saidaMux3, saidaSomador1, saidaSomador2: signed (7 downto 0);
+  signal saidaRegA, saidaRegS, saidaRegD, saidaShift, saidaMux4: signed (7 downto 0);
+  begin
+  regA: Registrador port map(A,reset,clock,saidaRegA);
+  regS: Registrador port map(saidaMux1, (itera or reset),clock,saidaRegS);
+  regD: Registrador port map(saidaMux2, (itera or reset),clock,saidaRegD);
+  regM: Registrador port map(saidaSomador2,done,clock,Saida);
+  mux1: Multiplexador port map(saidaSomador1,"00000001",reset,saidaMux1);
+  mux2: Multiplexador port map(saidaSomador2,"00000011",reset,saidaMux2);
+  mux3: Multiplexador port map("00000010","00000001",done,saidaMux3);
+  mux4: Multiplexador port map(saidaRegD,saidaShift,done,saidaMux4);
+  shft: ShiftRight port map(saidaRegD, saidaShift);
+  sum1: Somador port map(saidaRegS,saidaRegD,'0',saidaSomador1);
+  sum2: Somador port map(saidaMux4,saidaMux3,done,saidaSomador2);
+  comp: Comparador port map(saidaRegS,saidaRegA,SgtA);
+end architecture;
+
+library ieee;
+use ieee.numeric_bit.all;
+
+-- @brief Raiz Quadrada
+entity square_uc is
+  port (
+    SgtA : in bit;
+    clock : in bit;
+
+    itera : out bit;
+    done : out bit
+  );
+end entity square_uc;
+
+architecture square_uc_arch of square_uc is
+  begin
+    my_process : process(clock)
+    begin
+			if(clock'event and clock='1')then
+			  done <= SgtA;
+			  itera <= not(SgtA);
 			end if;
-		end process;
-	end architecture;
-
-
-	library ieee;
-	use ieee.numeric_bit.all;
-
-	entity comparador is
-	  port (
-			A : in  signed(7 downto 0);
-			B : in  signed(7 downto 0);
-			S: out bit
-	  );
-	end entity comparador;
-
-	architecture hardware of comparador is
-	begin
-
-		S <= '1' when A > B else '0';
-
-
-	end architecture;
-
-	library ieee;
-	use ieee.numeric_bit.all;
-
-	entity reg8 is
-	  port (
-			E : in  signed(7 downto 0);
-			S : out  signed(7 downto 0);
-			load: in bit;
-			reset : in  bit; -- reset ativo alto assíncrono
-			clk   : in  bit
-	  );
-	end entity reg8;
-
-	architecture hardware of reg8 is
-	begin
-		process(clk,reset,load)
-		 begin
-			if reset = '1' then
-			  s <= "00000000";
-			elsif(clk'event and clk = '1') then
-				if load = '1' then S <= E; end if;
-			end if;
-		end process;
-
-	end architecture;
-
-	library ieee;
-	use ieee.numeric_bit.all;
-
-	entity mux2to1 is
-	  port (
-			SEL : in  bit;
-			A : in  signed(7 downto 0);
-			B : in  signed(7 downto 0);
-			S : out  signed(7 downto 0)
-	  );
-	end entity mux2to1;
-
-	architecture hardware of mux2to1 is
-	begin
-		S <= A when (SEL = '0') else B;
-
-	end architecture;
-
-
-	library ieee;
-	use ieee.numeric_bit.all;
-
-	entity shift2 is
-	  port (
-			e : in  signed(7 downto 0);
-			S : out  signed(7 downto 0)
-	  );
-	end entity shift2;
-
-	architecture hardware of shift2 is
-	begin
-		S <= "0" & E(7 downto 1);
-
-	end architecture;
+	  end process;
+end architecture;
